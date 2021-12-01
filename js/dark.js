45d911f168d7d4d0c4bec7b2574d5aa82bd6f7ea
FA2500 <mfarisammar@gmail.com>
2021-12-01 23:39:25 +0800

    Update dark.js

--- js/dark.js
+++ js/dark.js
@@ -1,4 +1,24 @@
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+
+  if(element.classList.contains("dark-mode"))
+  {
+    localStorage.setItem('isDarkMode',"dark-mode");
+  }
+  else
+  {
+    localStorage.setItem('isDarkMode',"");
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  //when loading document
+        var background = localStorage.getItem("isDarkMode");
+      // get localStorage var background
+        if (background) {
+      // if its not null and empty
+          document.body.className += background;
+      //add class to body
+        }
+      });
